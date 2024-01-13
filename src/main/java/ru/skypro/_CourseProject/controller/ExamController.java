2141ca009a8d1788d40c816fb1ad3bfa1a0fa437
Solpostgit <begemot_off@mail.ru>
2024-01-13 09:12:11 +0300

    added ExamController

---
+++ src/main/java/ru/skypro/_CourseProject/controller/ExamController.java
@@ -0,0 +1,10 @@
+package ru.skypro._CourseProject.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExamController {
+    public void getQuestions(int amount) {
+        return ;
+    }
+}
